dded6e491e8231094f00180957895073bcd5c7e3
yanguadotdev <yanguasamir05@gmail.com>
2025-06-04 10:00:03 -0500

    refactor: add removeInlineStyles util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,12 +10,27 @@ interface CumulativeTransitionDelayProps {
   delayPattern: number,
   start?: number,
   bottomToTop?: boolean,
-  sameForAll?: boolean
+  sameForAll?: boolean,
+  duration?: number
 }
-export function applyCumulativeTransitionDelay({ items, delayPattern, start = 0, bottomToTop = false, sameForAll = false }: CumulativeTransitionDelayProps) {
+export function applyCumulativeTransitionDelay({ items, delayPattern, start = 0, bottomToTop = false, sameForAll = false, duration }: CumulativeTransitionDelayProps) {
   items.forEach((item, index) => {
     const position = bottomToTop ? items.length - index : index + 1;
     const delay = sameForAll ? delayPattern : position * delayPattern;
     item.style.transitionDelay = `${delay + start}s`;
+    item.style.transitionDuration = `${duration}s`
   });
+}
+
+
+export function removeInlineStyles(
+  items: HTMLElement[],
+  ...properties: (keyof CSSStyleDeclaration)[]
+) {
+  items.forEach((item) => {
+    item.style.transitionDelay = '0s'
+    properties.forEach((property) => {
+      ; (item.style as any)[property] = ''
+    })
+  })
 }
