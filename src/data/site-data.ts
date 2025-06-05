6418c59c11a61f5f9f2e0f2fd4137681645a2199
yanguadotdev <yanguasamir05@gmail.com>
2025-06-04 22:55:04 -0500

    feat: add classes data structure with categories and available classes for enhanced user offerings

--- src/data/site-data.ts
+++ src/data/site-data.ts
@@ -32,3 +32,76 @@ export const benefits = [
     img: '/images/luxury-amenities.webp'
   }
 ]
+
+
+export const classes = [
+  {
+    category: 'exclusive studio',
+    classesAvailable: [
+      {
+        name: 'trx',
+        goal: 'get stronger',
+        duration: '45 mins',
+        membership: 'exclusive',
+      },
+      {
+        name: 'fly-yoga',
+        goal: 'get fitter',
+        duration: '01 hour',
+        membership: 'exclusive',
+      },
+      {
+        name: 'cicle',
+        goal: 'get stronger',
+        duration: '45 mins',
+        membership: 'exclusive',
+      },
+    ],
+  },
+  {
+    category: 'group training',
+    classesAvailable: [
+      {
+        name: 'bootcamp',
+        goal: 'burn fat',
+        duration: '50 mins',
+        membership: 'all inclusive',
+      },
+      {
+        name: 'hiit cardio',
+        goal: 'improve endurance',
+        duration: '45 mins',
+        membership: 'super',
+      },
+      {
+        name: 'zumba',
+        goal: 'have fun',
+        duration: '01 hour',
+        membership: 'all inclusive',
+      },
+    ],
+  },
+  {
+    category: 'mind & body',
+    classesAvailable: [
+      {
+        name: 'vinyasa yoga',
+        goal: 'improve flexibility',
+        duration: '01 hour',
+        membership: 'super',
+      },
+      {
+        name: 'pilates',
+        goal: 'tone your core',
+        duration: '50 mins',
+        membership: 'all inclusive',
+      },
+      {
+        name: 'breathwork',
+        goal: 'reduce stress',
+        duration: '30 mins',
+        membership: 'super',
+      },
+    ],
+  },
+]
